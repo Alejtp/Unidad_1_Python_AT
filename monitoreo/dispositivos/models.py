5748a690e2856df663148938f3c03544d6a52818
Alejtp <alejandro.tapia38@inacapmail.cl>
2025-08-27 12:44:52 -0400

    segundo commit

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,11 +1,39 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+class Zona(models.Model):
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
 
 class Dispositivo(models.Model):
     nombre = models.CharField(max_length=100)
-    consumo = models.IntegerField()
     estado = models.BooleanField(default=True)
 
     def __str__(self):
         return self.nombre
-    # Create your models here.
+    
+class DispoElectrico(models.Model):
+    nombre = models.CharField(max_length=100)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Medicion(models.Model):
+    nombre = models.CharField(max_length=100)
+    consumo = models.IntegerField()
+
+    def __str__(self):
+        return self.consumo()
+
+
